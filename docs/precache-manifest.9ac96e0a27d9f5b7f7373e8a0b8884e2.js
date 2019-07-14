9183a2ee905506930d0088461a9dc9cb679dd01e
Ace <504595380@qq.com>
2019-07-14 21:57:31 +0800

    试试看 docs 能不能访问

---
+++ docs/precache-manifest.9ac96e0a27d9f5b7f7373e8a0b8884e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "d2d02e677f4026e2f6f9",
+    "url": "js/chunk-vendors.2998fc38.js"
+  },
+  {
+    "revision": "3aef912f84c11956c236",
+    "url": "js/app.70f2ccea.js"
+  },
+  {
+    "revision": "75ac65568744ea211088",
+    "url": "js/about.975f01c0.js"
+  },
+  {
+    "revision": "eefafc53fbb690614d3fa494ae72f073",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2607abfd0e454e01a0375e2b6d4385db",
+    "url": "data/json/北京联合大学生物化学工程学院.json"
+  },
+  {
+    "revision": "9e9ed88da163cc3cb8a8895b7954959d",
+    "url": "data/json/stu.json"
+  },
+  {
+    "revision": "3aef912f84c11956c236",
+    "url": "css/app.0736bfc7.css"
+  }
+];
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // 基本路径
+  publicPath: '',
+  assetsDir: './',
+  outputDir: 'docs',
+
+}
