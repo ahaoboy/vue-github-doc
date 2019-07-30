96ac9999d525b81cda8c0b0aff8c03b674085628
Ace <504595380@qq.com>
2019-07-30 21:21:09 +0800

    添加分数查询数据

---
+++ docs/js/app.a424b1be.js
@@ -0,0 +1,696 @@
+(function(e) {
+  function t(t) {
+    for (
+      var n, l, s = t[0], i = t[1], u = t[2], c = 0, f = [];
+      c < s.length;
+      c++
+    )
+      (l = s[c]), o[l] && f.push(o[l][0]), (o[l] = 0);
+    for (n in i) Object.prototype.hasOwnProperty.call(i, n) && (e[n] = i[n]);
+    p && p(t);
+    while (f.length) f.shift()();
+    return a.push.apply(a, u || []), r();
+  }
+  function r() {
+    for (var e, t = 0; t < a.length; t++) {
+      for (var r = a[t], n = !0, l = 1; l < r.length; l++) {
+        var i = r[l];
+        0 !== o[i] && (n = !1);
+      }
+      n && (a.splice(t--, 1), (e = s((s.s = r[0]))));
+    }
+    return e;
+  }
+  var n = {},
+    o = { app: 0 },
+    a = [];
+  function l(e) {
+    return (
+      s.p +
+      "js/" +
+      ({ about: "about" }[e] || e) +
+      "." +
+      { about: "ce39a57a" }[e] +
+      ".js"
+    );
+  }
+  function s(t) {
+    if (n[t]) return n[t].exports;
+    var r = (n[t] = { i: t, l: !1, exports: {} });
+    return e[t].call(r.exports, r, r.exports, s), (r.l = !0), r.exports;
+  }
+  (s.e = function(e) {
+    var t = [],
+      r = o[e];
+    if (0 !== r)
+      if (r) t.push(r[2]);
+      else {
+        var n = new Promise(function(t, n) {
+          r = o[e] = [t, n];
+        });
+        t.push((r[2] = n));
+        var a,
+          i = document.createElement("script");
+        (i.charset = "utf-8"),
+          (i.timeout = 120),
+          s.nc && i.setAttribute("nonce", s.nc),
+          (i.src = l(e)),
+          (a = function(t) {
+            (i.onerror = i.onload = null), clearTimeout(u);
+            var r = o[e];
+            if (0 !== r) {
+              if (r) {
+                var n = t && ("load" === t.type ? "missing" : t.type),
+                  a = t && t.target && t.target.src,
+                  l = new Error(
+                    "Loading chunk " + e + " failed.\n(" + n + ": " + a + ")"
+                  );
+                (l.type = n), (l.request = a), r[1](l);
+              }
+              o[e] = void 0;
+            }
+          });
+        var u = setTimeout(function() {
+          a({ type: "timeout", target: i });
+        }, 12e4);
+        (i.onerror = i.onload = a), document.head.appendChild(i);
+      }
+    return Promise.all(t);
+  }),
+    (s.m = e),
+    (s.c = n),
+    (s.d = function(e, t, r) {
+      s.o(e, t) || Object.defineProperty(e, t, { enumerable: !0, get: r });
+    }),
+    (s.r = function(e) {
+      "undefined" !== typeof Symbol &&
+        Symbol.toStringTag &&
+        Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }),
+        Object.defineProperty(e, "__esModule", { value: !0 });
+    }),
+    (s.t = function(e, t) {
+      if ((1 & t && (e = s(e)), 8 & t)) return e;
+      if (4 & t && "object" === typeof e && e && e.__esModule) return e;
+      var r = Object.create(null);
+      if (
+        (s.r(r),
+        Object.defineProperty(r, "default", { enumerable: !0, value: e }),
+        2 & t && "string" != typeof e)
+      )
+        for (var n in e)
+          s.d(
+            r,
+            n,
+            function(t) {
+              return e[t];
+            }.bind(null, n)
+          );
+      return r;
+    }),
+    (s.n = function(e) {
+      var t =
+        e && e.__esModule
+          ? function() {
+              return e["default"];
+            }
+          : function() {
+              return e;
+            };
+      return s.d(t, "a", t), t;
+    }),
+    (s.o = function(e, t) {
+      return Object.prototype.hasOwnProperty.call(e, t);
+    }),
+    (s.p = ""),
+    (s.oe = function(e) {
+      throw (console.error(e), e);
+    });
+  var i = (window["webpackJsonp"] = window["webpackJsonp"] || []),
+    u = i.push.bind(i);
+  (i.push = t), (i = i.slice());
+  for (var c = 0; c < i.length; c++) t(i[c]);
+  var p = u;
+  a.push([0, "chunk-vendors"]), r();
+})({
+  0: function(e, t, r) {
+    e.exports = r("56d7");
+  },
+  "034f": function(e, t, r) {
+    "use strict";
+    var n = r("64a9"),
+      o = r.n(n);
+    o.a;
+  },
+  "53ee": function(e, t, r) {
+    "use strict";
+    var n = r("ff9f"),
+      o = r.n(n);
+    o.a;
+  },
+  "56d7": function(e, t, r) {
+    "use strict";
+    r.r(t);
+    r("cadf"), r("551c"), r("f751"), r("097d");
+    var n,
+      o = r("2b0e"),
+      a = function() {
+        var e = this,
+          t = e.$createElement,
+          r = e._self._c || t;
+        return r(
+          "div",
+          { attrs: { id: "app" } },
+          [
+            r(
+              "div",
+              { staticClass: "form" },
+              [
+                r(
+                  "el-select",
+                  {
+                    staticStyle: { width: "100px" },
+                    model: {
+                      value: e.type,
+                      callback: function(t) {
+                        e.type = t;
+                      },
+                      expression: "type"
+                    }
+                  },
+                  [
+                    r("el-option", { attrs: { label: "理科", value: "理科" } }),
+                    r("el-option", { attrs: { label: "文科", value: "文科" } })
+                  ],
+                  1
+                ),
+                r("el-input-number", {
+                  staticStyle: { margin: "0 15px" },
+                  attrs: { min: 400, max: 700 },
+                  model: {
+                    value: e.score,
+                    callback: function(t) {
+                      e.score = t;
+                    },
+                    expression: "score"
+                  }
+                }),
+                r("el-button", { on: { click: e.init } }, [e._v("查询")])
+              ],
+              1
+            ),
+            r(
+              "el-table",
+              {
+                staticStyle: { width: "100%" },
+                attrs: {
+                  fit: "true",
+                  border: "",
+                  "highlight-current-row": !0,
+                  data: e.dataList,
+                  "max-height": "500"
+                }
+              },
+              [
+                r("el-table-column", {
+                  attrs: {
+                    prop: "schoolName",
+                    label: "学校",
+                    sortable: "",
+                    align: "center"
+                  }
+                }),
+                r("el-table-column", {
+                  attrs: {
+                    prop: "spname",
+                    label: "专业",
+                    sortable: "",
+                    align: "center"
+                  }
+                }),
+                r("el-table-column", {
+                  attrs: {
+                    prop: "min",
+                    label: "最低分",
+                    sortable: "",
+                    align: "center",
+                    width: "150px"
+                  }
+                }),
+                r("el-table-column", {
+                  attrs: {
+                    prop: "average",
+                    label: "平均分",
+                    sortable: "",
+                    align: "center",
+                    width: "150px"
+                  }
+                }),
+                r("el-table-column", {
+                  attrs: {
+                    prop: "max",
+                    label: "最高分",
+                    sortable: "",
+                    align: "center",
+                    width: "150px"
+                  }
+                })
+              ],
+              1
+            )
+          ],
+          1
+        );
+      },
+      l = [],
+      s = (r("96cf"), r("3b8d")),
+      i = r("bc3a"),
+      u = r.n(i),
+      c = r("5c96"),
+      p = r.n(c);
+    function f() {
+      n = c["Loading"].service({
+        lock: !0,
+        text: "加载中...",
+        background: "deepgray"
+      });
+    }
+    function v() {
+      n.close();
+    }
+    u.a.interceptors.request.use(
+      function(e) {
+        return f(), e;
+      },
+      function(e) {
+        return Promise.reject(e);
+      }
+    ),
+      u.a.interceptors.response.use(
+        function(e) {
+          return v(), e.data;
+        },
+        function(e) {
+          return v(), c["Message"].error("加载错误"), Promise.reject(e);
+        }
+      );
+    var h = u.a;
+    function d(e, t, r, n) {
+      return h.get(
+        "https://ahaoboy.github.io/vue-github-doc/json/"
+          .concat(e, "/")
+          .concat(t, "/")
+          .concat(r, "/")
+          .concat(n, ".json")
+      );
+    }
+    var g = {
+        data: function() {
+          return { dataList: [], score: 600, type: "理科" };
+        },
+        methods: {
+          init: (function() {
+            var e = Object(s["a"])(
+              regeneratorRuntime.mark(function e() {
+                return regeneratorRuntime.wrap(
+                  function(e) {
+                    while (1)
+                      switch ((e.prev = e.next)) {
+                        case 0:
+                          return (
+                            (e.next = 2),
+                            d(this.type, "陕西", "2017", this.score)
+                          );
+                        case 2:
+                          this.dataList = e.sent;
+                        case 3:
+                        case "end":
+                          return e.stop();
+                      }
+                  },
+                  e,
+                  this
+                );
+              })
+            );
+            function t() {
+              return e.apply(this, arguments);
+            }
+            return t;
+          })()
+        },
+        created: function() {
+          this.init();
+        }
+      },
+      b = g,
+      m = (r("034f"), r("2877")),
+      _ = Object(m["a"])(b, a, l, !1, null, null, null),
+      w = _.exports,
+      y = r("8c4f"),
+      j = function() {
+        var e = this,
+          t = e.$createElement,
+          n = e._self._c || t;
+        return n(
+          "div",
+          { staticClass: "home" },
+          [
+            n("img", { attrs: { alt: "Vue logo", src: r("cf05") } }),
+            n("HelloWorld", { attrs: { msg: "Welcome to Your Vue.js App" } })
+          ],
+          1
+        );
+      },
+      k = [],
+      x = function() {
+        var e = this,
+          t = e.$createElement,
+          r = e._self._c || t;
+        return r("div", { staticClass: "hello" }, [
+          r("h1", [e._v(e._s(e.msg))]),
+          e._m(0),
+          r("h3", [e._v("Installed CLI Plugins")]),
+          e._m(1),
+          r("h3", [e._v("Essential Links")]),
+          e._m(2),
+          r("h3", [e._v("Ecosystem")]),
+          e._m(3)
+        ]);
+      },
+      O = [
+        function() {
+          var e = this,
+            t = e.$createElement,
+            r = e._self._c || t;
+          return r("p", [
+            e._v(
+              "\n    For a guide and recipes on how to configure / customize this project,"
+            ),
+            r("br"),
+            e._v("\n    check out the\n    "),
+            r(
+              "a",
+              {
+                attrs: {
+                  href: "https://cli.vuejs.org",
+                  target: "_blank",
+                  rel: "noopener"
+                }
+              },
+              [e._v("vue-cli documentation")]
+            ),
+            e._v(".\n  ")
+          ]);
+        },
+        function() {
+          var e = this,
+            t = e.$createElement,
+            r = e._self._c || t;
+          return r("ul", [
+            r("li", [
+              r(
+                "a",
+                {
+                  attrs: {
+                    href:
+                      "https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel",
+                    target: "_blank",
+                    rel: "noopener"
+                  }
+                },
+                [e._v("babel")]
+              )
+            ]),
+            r("li", [
+              r(
+                "a",
+                {
+                  attrs: {
+                    href:
+                      "https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa",
+                    target: "_blank",
+                    rel: "noopener"
+                  }
+                },
+                [e._v("pwa")]
+              )
+            ]),
+            r("li", [
+              r(
+                "a",
+                {
+                  attrs: {
+                    href:
+                      "https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint",
+                    target: "_blank",
+                    rel: "noopener"
+                  }
+                },
+                [e._v("eslint")]
+              )
+            ])
+          ]);
+        },
+        function() {
+          var e = this,
+            t = e.$createElement,
+            r = e._self._c || t;
+          return r("ul", [
+            r("li", [
+              r(
+                "a",
+                {
+                  attrs: {
+                    href: "https://vuejs.org",
+                    target: "_blank",
+                    rel: "noopener"
+                  }
+                },
+                [e._v("Core Docs")]
+              )
+            ]),
+            r("li", [
+              r(
+                "a",
+                {
+                  attrs: {
+                    href: "https://forum.vuejs.org",
+                    target: "_blank",
+                    rel: "noopener"
+                  }
+                },
+                [e._v("Forum")]
+              )
+            ]),
+            r("li", [
+              r(
+                "a",
+                {
+                  attrs: {
+                    href: "https://chat.vuejs.org",
+                    target: "_blank",
+                    rel: "noopener"
+                  }
+                },
+                [e._v("Community Chat")]
+              )
+            ]),
+            r("li", [
+              r(
+                "a",
+                {
+                  attrs: {
+                    href: "https://twitter.com/vuejs",
+                    target: "_blank",
+                    rel: "noopener"
+                  }
+                },
+                [e._v("Twitter")]
+              )
+            ]),
+            r("li", [
+              r(
+                "a",
+                {
+                  attrs: {
+                    href: "https://news.vuejs.org",
+                    target: "_blank",
+                    rel: "noopener"
+                  }
+                },
+                [e._v("News")]
+              )
+            ])
+          ]);
+        },
+        function() {
+          var e = this,
+            t = e.$createElement,
+            r = e._self._c || t;
+          return r("ul", [
+            r("li", [
+              r(
+                "a",
+                {
+                  attrs: {
+                    href: "https://router.vuejs.org",
+                    target: "_blank",
+                    rel: "noopener"
+                  }
+                },
+                [e._v("vue-router")]
+              )
+            ]),
+            r("li", [
+              r(
+                "a",
+                {
+                  attrs: {
+                    href: "https://vuex.vuejs.org",
+                    target: "_blank",
+                    rel: "noopener"
+                  }
+                },
+                [e._v("vuex")]
+              )
+            ]),
+            r("li", [
+              r(
+                "a",
+                {
+                  attrs: {
+                    href: "https://github.com/vuejs/vue-devtools#vue-devtools",
+                    target: "_blank",
+                    rel: "noopener"
+                  }
+                },
+                [e._v("vue-devtools")]
+              )
+            ]),
+            r("li", [
+              r(
+                "a",
+                {
+                  attrs: {
+                    href: "https://vue-loader.vuejs.org",
+                    target: "_blank",
+                    rel: "noopener"
+                  }
+                },
+                [e._v("vue-loader")]
+              )
+            ]),
+            r("li", [
+              r(
+                "a",
+                {
+                  attrs: {
+                    href: "https://github.com/vuejs/awesome-vue",
+                    target: "_blank",
+                    rel: "noopener"
+                  }
+                },
+                [e._v("awesome-vue")]
+              )
+            ])
+          ]);
+        }
+      ];
+    r("ac4d"), r("8a81"), r("ac6a");
+    u.a.get("/data/json/stu.json").then(function(e) {
+      var t = e.data;
+      console.log(t);
+      var r = !0,
+        n = !1,
+        o = void 0;
+      try {
+        for (
+          var a, l = t[Symbol.iterator]();
+          !(r = (a = l.next()).done);
+          r = !0
+        ) {
+          var s = a.value;
+          console.log(s);
+        }
+      } catch (i) {
+        (n = !0), (o = i);
+      } finally {
+        try {
+          r || null == l.return || l.return();
+        } finally {
+          if (n) throw o;
+        }
+      }
+    });
+    var E = { name: "HelloWorld", props: { msg: String } },
+      S = E,
+      P = (r("53ee"), Object(m["a"])(S, x, O, !1, null, "1e20a206", null)),
+      C = P.exports,
+      $ = { name: "home", components: { HelloWorld: C } },
+      L = $,
+      T = Object(m["a"])(L, j, k, !1, null, null, null),
+      A = T.exports;
+    o["default"].use(y["a"]);
+    var M = new y["a"]({
+        mode: "history",
+        base: "",
+        routes: [
+          { path: "/", name: "home", component: A },
+          {
+            path: "/about",
+            name: "about",
+            component: function() {
+              return r.e("about").then(r.bind(null, "f820"));
+            }
+          }
+        ]
+      }),
+      N = r("2f62");
+    o["default"].use(N["a"]);
+    var F = new N["a"].Store({ state: {}, mutations: {}, actions: {} }),
+      W = r("9483");
+    Object(W["a"])("".concat("", "service-worker.js"), {
+      ready: function() {
+        console.log(
+          "App is being served from cache by a service worker.\nFor more details, visit https://goo.gl/AFskqB"
+        );
+      },
+      registered: function() {
+        console.log("Service worker has been registered.");
+      },
+      cached: function() {
+        console.log("Content has been cached for offline use.");
+      },
+      updatefound: function() {
+        console.log("New content is downloading.");
+      },
+      updated: function() {
+        console.log("New content is available; please refresh.");
+      },
+      offline: function() {
+        console.log(
+          "No internet connection found. App is running in offline mode."
+        );
+      },
+      error: function(e) {
+        console.error("Error during service worker registration:", e);
+      }
+    });
+    r("0fae");
+    o["default"].use(p.a),
+      (o["default"].config.productionTip = !1),
+      new o["default"]({
+        router: M,
+        store: F,
+        render: function(e) {
+          return e(w);
+        }
+      }).$mount("#app");
+  },
+  "64a9": function(e, t, r) {},
+  cf05: function(e, t, r) {
+    e.exports = r.p + "img/logo.82b9c7a5.png";
+  },
+  ff9f: function(e, t, r) {}
+});
+//# sourceMappingURL=app.a424b1be.js.map
